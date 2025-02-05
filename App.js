bc6e8d120702ef714fdec74466cf607b14b3dd26
Sandeep Patange <sandeep.upatange@gmail.com>
2025-02-05 10:55:51 +0530

    Add ImageBackground to App component along with LinearGradient

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 // Purpose: Main entry point for the application. This is where the app starts and renders the StartGameScreen component.
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient"; // Import the LinearGradient component from the expo-linear-gradient library
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.jpg")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.imagebackground}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  imagebackground: {
+    opacity: 0.2,
   },
 });
